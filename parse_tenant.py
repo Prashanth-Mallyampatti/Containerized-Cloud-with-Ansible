c27ec5be4501f430a207e8cd3f7ccb563953c35b
Prashanth-Mallyampatti <prashanth.m1996@gmail.com>
2019-11-28 23:56:23 +0000

    tenant parser

--- parse_tenant.py
+++ parse_tenant.py
@@ -3,35 +3,54 @@
 # and creates the required files for Ansible
 #################################################
 
-import ipaddress
-import yaml
-import os
 import sys
-import datetime
-import logging
+import yaml
 import csv
 
 main = {}
 tenant = []
 
-# Parse the input CSV file and generate a yaml file.
+# Parse the input CSV file
 CSV_file = "./" + str(sys.argv[1])
 with open(CSV_file, mode='r') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',' )
     line_count = 0
-
+ 
     for row in csv_reader:
         tenant_list = {}
-        #print (row[2])
         line_count += 1
         tenant_list["container_1"] = row[0]
         tenant_list["container_2"] = row[1]
         tenant_list["network_type"] = row[2]
-        #print (container_1, container_2, network_type)
+        
+        if row[2] == "Bridge" or row[2] == "L2":
+            tenant_list["namespace_1"] = "ns2"
+            tenant_list["namespace_2"] = "ns2"
+            
+        if row[2] == "L3":
+            tenant_list["namespace_1"] = "ns2"
+            tenant_list["namespace_2"] = "ns3"
+
+        if row[2] == "VXLAN":
+            tenant_list["namespace_1"] = "ns1"
+            tenant_list["namespace_2"] = "ns3"
+
+        if row[2] == "GRE":
+            tenant_list["namespace_1"] = "ns1"
+            tenant_list["namespace_2"] = "ns2"
+
+        tenant_list["c1_IP"] = "10.10." + str(line_count) + ".1/24" 
+        tenant_list["c2_IP"] = "10.10." + str(line_count) + ".2/24" 
+
+        tenant_list["C1_veth"] = row[0] + "c1"
+        tenant_list["C2_veth"] = row[1] + "c2"
+        tenant_list["ns1_veth"] = row[0] + "n1"
+        tenant_list["ns2_veth"] = row[1] + "n2"
+
         tenant.append(tenant_list)
 
-    #print (tenant)
-    print (line_count)
-
     main["tenant"] = tenant
-    print (main)
+
+# Creating yaml file
+with open("./tenant.yml", "w") as file:
+    doc = yaml.dump(main, file, default_flow_style=False)
